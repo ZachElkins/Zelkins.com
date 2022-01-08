ac5b30ca22f620de6621da296abcb8c6f6af711f
ZachElkins <zach.elkins@gmil.com>
2022-01-08 14:45:24 -0700

    links file

---
+++ links.js
@@ -0,0 +1,18 @@
+const links = [
+    {
+        url: "https://www.linkedin.com/in/zachary-elkins-b70504131/",
+        text: "Linked-In"
+    },
+    {
+        url: "https://github.com/ZachElkins",
+        text: "GitHub"
+    },
+    {
+        url: "./resume.pdf",
+        text: "Resume"
+    },
+    {
+        url: "https://zachelkins.itch.io",
+        text: "Itch.io"
+    }
+];
